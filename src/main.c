fb523b2ecd84427043f1aa36e6fb8bd9fe118c1d
jaewon <jaewon@jaewon-laptop.(none)>
2010-04-30 22:57:21 +0900

    added configure_file

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include "config.h"
 #include "show.h"
 
 int main(int argc, char *argv[])
 {
+#ifdef ARG
+ printf("ARG : %d\n", ARG);
+#endif
+
  printf("%s():%d\n", __FUNCTION__, __LINE__);
  show();
 
